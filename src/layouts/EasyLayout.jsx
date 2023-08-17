7b3abd14547556e645382e67680ccbd2bf96441d
Olga <osarcher@gmail.com>
2023-08-17 15:13:49 +0300

    backBtn added to easy

--- src/layouts/EasyLayout.jsx
+++ src/layouts/EasyLayout.jsx
@@ -1,15 +1,15 @@
 import { Outlet, useOutletContext } from "react-router-dom";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
-// const tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;
 
 export default function EasyLayout() {
     const data = useOutletContext();
     const phrases = data.filter((phrase) => phrase.level === "easy");
 
-    // useEffect(() => {
-    //     tg.BackButton.show();
-    // }, []);
+    useEffect(() => {
+        tg.BackButton.show();
+    }, []);
 
     return (
         <div className="quiz">
