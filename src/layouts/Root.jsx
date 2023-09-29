5a514a553cef6b8d3964a568e2cb7ae06a2771b9
Olga <osarcher@gmail.com>
2023-09-29 23:11:22 +0300

    hi user

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -2,12 +2,11 @@ import { Outlet, useLocation } from "react-router-dom";
 import { phrases } from "~/phrases";
 
 const tg = window.Telegram.WebApp;
-const initData = tg.initData;
 
 function Root() {
     const location = useLocation();
     const currentPath = location.pathname;
-    console.log(initData);
+
     if (currentPath === "/") {
         tg.BackButton.hide();
     }
