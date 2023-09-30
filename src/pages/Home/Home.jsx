97302ad028fa2a9c8c07b5d368cd1c7cf2267199
Olga <osarcher@gmail.com>
2023-09-30 14:15:30 +0300

    init data if

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 const tg = window.Telegram.WebApp;
 
 const Home = () => {
+    console.log("initDataUnsafe", tg.initDataUnsafe);
     return (
         <>
-            Hi {tg.initDataUnsafe.user.id}!
+            {tg.initDataUnsafe.length > 0 ? (
+                <p>Hi {tg.initDataUnsafe.user.id}!</p>
+            ) : (
+                <p>Hi user!</p>
+            )}
             <div className="container">
                 <h3>Уровень:</h3>
                 <Link to="easy">
