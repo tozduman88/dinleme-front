65c580aa6f8dd12005cd1838711da10a8bb9754c
Olga <osarcher@gmail.com>
2023-09-30 14:32:15 +0300

    value of length

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 const tg = window.Telegram.WebApp;
 
 const Home = () => {
+    console.log("length", tg.initDataUnsafe.valueOf.length);
     return (
         <>
-            {tg.initDataUnsafe.length > 0 ? (
+            {tg.initDataUnsafe.valueOf.length > 0 ? (
                 <Link to={tg.initDataUnsafe.user.id}>Мой профиль</Link>
             ) : (
                 <p>Hi user!</p>
