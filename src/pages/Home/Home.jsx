b251c7b844ec5b6ba3beff0c33f3786869b2244b
Olga <osarcher@gmail.com>
2023-10-01 19:38:19 +0300

    user params

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,16 +2,17 @@ import { MenuButton } from "../../components/MenuButton";
 import { Link } from "react-router-dom";
 
 const tg = window.Telegram.WebApp;
-const hash = window.location.hash.slice(1);
-console.log("hash", hash);
-const params = new URLSearchParams(hash);
-const initData = new URLSearchParams(params.get("tgWebAppData"));
+
+const params = new URLSearchParams(window.location.hash.slice(1));
+const initDataString = params.get("tgWebAppData");
+const initData = new URLSearchParams(initDataString);
+const user = initData.get("user");
 
 const Home = () => {
     const length = Object.keys(tg.initDataUnsafe).length;
     return (
         <>
-            <p>{initData.user}</p>
+            <p>{user}</p>
             {length > 0 ? (
                 <Link to={`profile/${tg.initDataUnsafe.user.id}`}>
                     Мой профиль
