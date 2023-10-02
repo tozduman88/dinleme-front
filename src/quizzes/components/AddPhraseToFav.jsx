8895a6864427ac9e9a02b9f16e6d3417b67defa8
Olga <osarcher@gmail.com>
2023-10-02 18:50:11 +0300

    datamy

--- src/quizzes/components/AddPhraseToFav.jsx
+++ src/quizzes/components/AddPhraseToFav.jsx
@@ -1,8 +1,10 @@
 import { addMyPhrase } from "~/utils/api";
 import WebApp from "@twa-dev/sdk";
 import { retrieveLaunchParams } from "@tma.js/sdk";
+import { initData } from "@tma.js/init-data";
 
 const initDataString = WebApp.initData;
+const myData = initData(initDataString);
 
 export default function AddPhraseToFav() {
     const add = async ({ phraseId }) => {
@@ -10,12 +12,12 @@ export default function AddPhraseToFav() {
             phrase_id: phraseId,
         };
 
-        return console.log("initData", initData(initDataString));
+        return;
         await addMyPhrase(data);
     };
     return (
         <>
-            {initDataString}
+            {myData}
             <button onClick={add}>Добавить в "Мои фразы"</button>
         </>
     );
