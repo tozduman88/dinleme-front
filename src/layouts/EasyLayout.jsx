43dd0fda860b6890514fcc54f91cd75d7558e9be
Olga <osarcher@gmail.com>
2023-10-03 15:56:38 +0300

    removed

--- src/layouts/EasyLayout.jsx
+++ src/layouts/EasyLayout.jsx
@@ -3,12 +3,11 @@ import { Outlet, useOutletContext } from "react-router-dom";
 export default function EasyLayout() {
     const data = useOutletContext();
     const phrases = data.phrases.filter((phrase) => phrase.level === "easy");
-    const currentUser = data.currentUser
+
 
     return (
 
         <div className="quiz">
-            {currentUser}
             <Outlet context={phrases} />
         </div>
     );
