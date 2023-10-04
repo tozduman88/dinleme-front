95f33d13cab1636aeb7dd5dd2159df5f5b1c83a1
Olga <osarcher@gmail.com>
2023-10-05 00:02:03 +0300

    hamburger

--- src/components/StickyUpperBar.jsx
+++ src/components/StickyUpperBar.jsx
@@ -1,10 +1,13 @@
 import styles from "./StickyUpperBar.module.css";
 import PropTypes from "prop-types";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function StickyUpperBar({ toggle }) {
     return (
         <div className={styles.bar}>
-            <button onClick={toggle}>toggle</button>
+            <button onClick={toggle}>
+                <GiHamburgerMenu className={styles.icon} />
+            </button>
         </div>
     );
 }
