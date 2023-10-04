af4a79990961a1a157eb6e4e4b98fdb176538cb6
Olga <osarcher@gmail.com>
2023-10-04 14:37:53 +0300

    navigate relative path easy

--- src/pages/Easy/EasyMenu.jsx
+++ src/pages/Easy/EasyMenu.jsx
@@ -1,19 +1,17 @@
 import QuizMenu from "~/components/QuizMenu";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-// import WebApp from "@twa-dev/sdk";
-import { BackButton } from "@tma.js/sdk";
-
-const backButton = new BackButton("6.3");
+import WebApp from "@twa-dev/sdk";
 
 export default function EasyMenu() {
     const navigate = useNavigate();
-
     useEffect(() => {
-        backButton.show();
+        WebApp.BackButton.show();
     }, []);
 
-    backButton.on("click", () => navigate("/listen-phrases"));
+    WebApp.onEvent("backButtonClicked", () =>
+        navigate("..", { relative: "path" })
+    );
 
     return <QuizMenu />;
 }
