ce3e7068ae60599e3206cd05fb9398608058b8c7
Olga <osarcher@gmail.com>
2023-10-04 14:28:31 +0300

    easy menu backBtn

--- src/pages/Easy/EasyMenu.jsx
+++ src/pages/Easy/EasyMenu.jsx
@@ -1,16 +1,19 @@
 import QuizMenu from "~/components/QuizMenu";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import WebApp from "@twa-dev/sdk";
+// import WebApp from "@twa-dev/sdk";
+import { BackButton } from "@tma.js/sdk";
+
+const backButton = new BackButton("6.3");
 
 export default function EasyMenu() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        WebApp.BackButton.show();
+        backButton.show();
     }, []);
 
-    WebApp.onEvent("backButtonClicked", () => navigate("listen-phrases"));
+    backButton.on("click", () => navigate("/listen-phrases"));
 
     return <QuizMenu />;
 }
